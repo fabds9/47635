c57f540ad50b097b6348c1d57d29d14bc846c7db
Fabrizio Simeone <fabriziosimeone@outlook.com>
2023-01-25 14:40:48 -0300

    Agregando_primeros_models

--- estudiantes/views.py
+++ estudiantes/views.py
@@ -1,10 +1,25 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
+
+from estudiantes.models import Estudiante
+
 
 def saludar(request):
     return HttpResponse(f'Hola comision 47635. Hora: {datetime.now()}')
 
 
 def listar_estudiantes(request):
-    return render(request=request, template_name='estudiantes/lista_estudiantes.html')
+    contexto = {
+        'estudiantes': Estudiante.objects.all()
+    }
+    return render(
+        request=request,
+        template_name='estudiantes/lista_estudiantes.html',
+        context=contexto
+    )
+
+
+def listar_profesores(request):
+    return render(request=request, template_name='estudiantes/lista_profesores.html')
